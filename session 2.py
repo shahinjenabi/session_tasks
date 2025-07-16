2776ba5888cf746381ba89ec7e6d59e263d5c922
shahinjenabi <mohammadrezajenabi620@gmail.com>
2025-07-16 21:31:30 +0330

    Add files via upload

---
+++ session 2.py
@@ -0,0 +1,115 @@
+# class_task_number_2
+
+# rectangle
+
+# rectagle_area
+
+y = 12
+
+x = 4.5
+
+z = x * y
+
+length = y
+
+width = x
+
+area = z
+
+print(area)
+
+print("x * y = ", z)
+
+# rectangle_perimeter
+
+y = 12
+
+x = 4.5 
+
+z = (y + x)*2
+
+length = y
+
+width = x 
+
+perimeter = z 
+
+print(perimeter)
+
+print("(y + x)*2 = ", z)
+
+# square
+
+# square_area
+
+y = 9
+
+z = y * y 
+
+squer_side = y
+
+area = z
+
+print("y * y = ", z)
+
+# square_perimeter
+
+y = 9 
+
+z = y * 4 
+
+square_side = y 
+
+perimeter = z 
+
+print("y * 4 = ", z)
+
+# circle
+
+# circle_area
+
+R = 6 
+
+P = 3.14
+
+z = R * R * P
+
+radius = R 
+
+number_Pi = P 
+
+area = z 
+
+print("R * R * P = ", z)
+
+# circle_perimeter
+
+R = 6 
+
+P = 3.14
+
+z = R * P
+
+radius = R 
+
+number_Pi = P 
+
+perimeter = z 
+
+print("R * P = ", z)
+
+# final_score
+
+A = 98.15
+
+B = 99.50
+
+Z = (A + B)/ 2 
+
+firs_exam = A 
+
+last_exam = B
+
+final_score = Z 
+
+print("(A + B)/ 2 =", Z)
